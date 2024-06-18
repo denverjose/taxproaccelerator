022b938c0267947c5b265f4f972381d3333459d1
denverjose <@dnvrjose6@gmail.com>
2024-06-19 01:14:52 +0800

    created breakpoint for fontsize of weather data

--- components/WeatherDisplay.tsx
+++ components/WeatherDisplay.tsx
@@ -1,17 +1,16 @@
 import { WeatherDisplayProps } from "../interfaces/weather";
 
 const WeatherDisplay: React.FC<WeatherDisplayProps> = ({ weather }) => {
-
-  if(!weather){
+  if (!weather) {
     return null;
   }
   return (
-    <div>
-      <h3 className="text-4xl">
+    <>
+      <h3 className="text-xl md:text-3xl">
         Weather in {weather?.name} is{" "}
         <span className="font-bold">{weather?.main.temp}°F</span>
       </h3>
-    </div>
+    </>
   );
 };
 
